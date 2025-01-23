970f3bde4acbb42c0fc3587d83dd51d6bf6918af
김의현 <keh0885@gmail.com>
2025-01-23 16:08:10 +0900

    feat: 공지사항 api 기능 구현 (BE-200)

---
+++ backend/src/main/java/com/h5/notice/repository/NoticeRepository.java
@@ -0,0 +1,36 @@
+package com.h5.notice.repository;
+
+import com.h5.notice.entity.NoticeEntity;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface NoticeRepository extends JpaRepository<NoticeEntity, Integer> {
+    //삭제안된 전체 글 + 페이징
+    Page<NoticeEntity> findAllByDeleteDttmIsNull(Pageable pageable);
+
+    //삭제 안된 글 제목으로 검색 + 페이징
+    @Query("SELECT n FROM NoticeEntity n WHERE n.title LIKE %:title% and n.deleteDttm is null")
+    Page<NoticeEntity> findByTitle(@NotNull String title, Pageable pageable);
+
+    //삭제 안된 글 작성자로 검색 + 페이징
+    @Query("SELECT n FROM NoticeEntity n WHERE n.consultantUser.email LIKE %:consultantUserEmail% and n.deleteDttm is null")
+    Page<NoticeEntity> findByEmail(@Size(max = 30) @NotNull String consultantUserEmail, Pageable pageable);
+
+    //상세 글 보기
+    NoticeEntity findById(int noticeId);
+
+    //조회수 증가
+    @Modifying
+    @Query("UPDATE NoticeEntity n SET n.viewCnt = n.viewCnt + 1 WHERE n.id = :noticeId")
+    void updateViewCnt (@Param("noticeId") int id);
+}
