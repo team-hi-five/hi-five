16fc1418e1f503b42cba932e1473dc467a09a5c0
정찬환 <chanhoan01@gmail.com>
2025-01-23 17:10:40 +0900

    fix: 로그인 api, spring security, jwt 빌드 버그 수정 (BE-100)

--- backend/src/main/java/com/h5/consultant/repository/ConsultantUserRepository.java
+++ backend/src/main/java/com/h5/consultant/repository/ConsultantUserRepository.java
@@ -3,6 +3,8 @@ package com.h5.consultant.repository;
 import com.h5.consultant.entity.ConsultantUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,6 @@ public interface ConsultantUserRepository extends JpaRepository<ConsultantUserEn
     Optional<ConsultantUserEntity> findByEmail(String email);
 
     @Modifying
-    int updateRefreshTokenByEmail(String email, String refreshToken);
+    @Query("UPDATE ConsultantUserEntity c SET c.refreshToken = :refreshToken WHERE c.email = :email")
+    int updateRefreshTokenByEmail(@Param("email") String email, @Param("refreshToken") String refreshToken);
 }
