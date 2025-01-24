80bdb5175dc7ac38432d73d4fe717188d5418514
정수연 <wjdtndus23@naver.com>
2025-01-24 17:24:24 +0900

    feat: 상담사 메인페이지 목업 구현 (FE-320)

--- frontend/h5/src/components/Counselor/CounselorHeader.jsx
+++ frontend/h5/src/components/Counselor/CounselorHeader.jsx
@@ -1,4 +1,5 @@
 import './CounselorHeader.css';
+import { Link } from 'react-router-dom';
 
 
 function CounselorHeader() {
@@ -8,7 +9,9 @@ function CounselorHeader() {
         <img src="/logo.png" alt="로고" className="co_logo" />
       </div>
       <ul className="co_nav-list">
-        <li className="co_nav-item">우리아이들</li>
+        <li className="co_nav-item">
+          <Link to="/counselor/children">우리아이들</Link>
+        </li>
         <li className="co_nav-item">상담일정</li>
         <li className="co_nav-item">게시판</li>
         <li className="co_nav-item">마이데이터</li>
