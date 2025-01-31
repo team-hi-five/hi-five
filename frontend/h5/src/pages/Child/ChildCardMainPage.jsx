cd48b242d4599f8647c4b809057b87baa68b12fa
seorinkim <seorin66@naver.com>
2025-01-31 17:27:03 +0900

    feat: 아동 카드리스트 뷰 구현 (FE-310)

--- frontend/h5/src/pages/Child/ChildCardMainPage.jsx
+++ frontend/h5/src/pages/Child/ChildCardMainPage.jsx
@@ -7,12 +7,20 @@ function ChildCardMainPage() {
         <div className='ch-card-main-container'>
             <div className='ch-card-details-nav'>
                 <Link to='joy'>
-                <img src="\test\기쁘미.PNG" alt="" />
-                joy</Link>
-                <Link to='sadness'>sadness</Link>
-                <Link to='anger'>anger</Link>
-                <Link to='fear'>fear</Link>
-                <Link to='surprise'>surprise</Link>
+                <img src="\test\기쁘미.PNG" alt="joy" />
+                </Link>
+                <Link to='sadness'>
+                <img src="\test\슬프미.png" alt="sadness" />
+                </Link>
+                <Link to='anger'>
+                <img src="\test\화나미.PNG" alt="anger" />
+                </Link>
+                <Link to='fear'>
+                <img src="\test\무서미.PNG" alt="fear" />
+                </Link>
+                <Link to='surprise'>
+                <img src="\test\놀라미.png" alt="surprise" />
+                </Link>
             </div>
             <Outlet/>
 
