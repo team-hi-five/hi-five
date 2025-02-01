fed071ff65b9bcf5f99622f9752cb67395200634
정찬환 <chanhoan01@gmail.com>
2025-02-02 00:11:03 +0900

    fix: 스웨거 테스트 및 버그 픽스

--- backend/src/main/java/com/h5/parent/service/ParentUserServiceImpl.java
+++ backend/src/main/java/com/h5/parent/service/ParentUserServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
