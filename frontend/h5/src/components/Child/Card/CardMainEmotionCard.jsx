b0538a6046101ad357ab9ea101d77c9b058ca3d1
seorinkim <seorin66@naver.com>
2025-02-02 23:12:28 +0900

    feat: 아동카드메인페이지 구현 (FE-310)

---
+++ frontend/h5/src/components/Child/Card/CardMainEmotionCard.jsx
@@ -0,0 +1,59 @@
+import '../ChildCss/CardMainEmotionCard.css'
+import { Card } from 'primereact/card'
+import PropTypes from 'prop-types'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+
+function CardMainEmotionCard ({ id, image, name, bgColor, textColor, onClick}){
+    
+    const [isSelected, setIsSelected] = useState(false);
+
+    // 더보기 클릭시 부모에게 클릭이벤트 전달 및 크기 확장
+    const handleClick = () => {
+        setIsSelected(true);
+        onClick(id);
+    }
+
+    return(
+        <motion.div className='ch-big-card-container'
+                    whileHover={isSelected? {} : {y : -50,transition: {duration:0.3}}}
+        >
+            <div className="ch-big-card"    
+                 onClick={handleClick}
+                 data-isselected={isSelected}
+            >
+                <img src={image} 
+                     alt={name}
+                     data-isselected={isSelected}
+                />
+                <Card className="ch-big-name-card"
+                      style={{ backgroundColor: bgColor }}
+                      data-isselected={isSelected}
+                >
+                    <h2 className="ch-big-card-name"
+                        style={{ color: textColor}}
+                        data-isselected={isSelected}
+                    >{name}</h2>
+                </Card>
+                <div className='card-hover-text'
+                     data-isselected={isSelected}
+                > 카드 더보기 &gt; </div>
+            </div>
+        </motion.div>
+    )
+
+   
+}
+
+// 타입 검증 경고메세지가 떠서 작성
+CardMainEmotionCard.propTypes = {
+    id:PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    bgColor: PropTypes.string.isRequired, 
+    textColor: PropTypes.string.isRequired,
+    onClick : PropTypes.func.isRequired,
+    
+}
+
+export default CardMainEmotionCard
