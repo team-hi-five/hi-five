4067181d05aae2cfaf437a3b3f2eb55c5a8c2212
seorin66 <seorin66@naver.com>
2025-02-04 08:28:42 +0900

    feat: 아동 카드 세부리스트 수정 중 (FE-310)

--- frontend/h5/src/components/Child/Card/CardMainEmotionCard.jsx
+++ frontend/h5/src/components/Child/Card/CardMainEmotionCard.jsx
@@ -1,54 +1,69 @@
-import '../ChildCss/CardMainEmotionCard.css'
-import { Card } from 'primereact/card'
-import PropTypes from 'prop-types'
-import { motion } from 'framer-motion'
-import { useState } from 'react'
+import "../ChildCss/CardMainEmotionCard.css";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-function CardMainEmotionCard ({ id, image, name, bgColor, textColor, onClick}){
-    
-    const [isSelected, setIsSelected] = useState(false);
+function CardMainEmotionCard({ id, image, name, bgColor, textColor, onClick }) {
+  const [isSelected, setIsSelected] = useState(false);
 
-    // 더보기 클릭시 부모에게 클릭이벤트 전달 및 크기 확장
-    const handleClick = () => {
-        setIsSelected(true);
-        onClick(id);
-    }
+  // 더보기 클릭시 부모에게 클릭이벤트 전달 및 크기 확장
+  const handleClick = () => {
+    setIsSelected(true);
+    // 애니메이션 완료 후 페이지 전환
+    setTimeout(() => {
+      // onClick() 실제로 페이지를 전환시키는 역할
+      onClick(id);
+    }, 500);
+  };
 
-    return(
-        <motion.div className='ch-big-card-container'
-                    whileHover={isSelected? {} : {y : -50,transition: {duration:0.3}}}
+  return (
+    // 호버효과
+    <motion.div
+      className="ch-big-card-container"
+      whileHover={isSelected ? {} : { y: -50, transition: { duration: 0.3 } }}
+    >
+      {/* 클릭시 카드 크기 증가 */}
+      <div className="ch-big-card" onClick={handleClick}>
+        <img src={image} alt={name} />
+        <motion.div
+          className="ch-big-name-card"
+          style={{ backgroundColor: bgColor }}
+          animate={
+            isSelected
+              ? {
+                  position: "fixed",
+                  scale: [1, 1.5, 3, 5],
+                  top: "0px",
+                  left: "0px",
+                  zIndex: 1000,
+                  borderRadius: ["100%", "20px", "0px"],
+                }
+              : {}
+          }
+          transition={{ duration: 1, ease: "easeInOut", times: [0, 0.2, 1] }}
         >
-            <div className="ch-big-card"    
-                 onClick={handleClick}
-            >
-                <img src={image} 
-                     alt={name}
-                />
-                <Card className="ch-big-name-card"
-                      style={{ backgroundColor: bgColor }}
-                >
-                    <h2 className="ch-big-card-name"
-                        style={{ color: textColor}}
-                    >{name}</h2>
-                </Card>
-                <div className='card-hover-text'
-                > 카드 더보기 &gt; </div>
-            </div>
+          <motion.h2
+            className="ch-big-card-name"
+            style={{ color: textColor }}
+            animate={isSelected ? { opacity: 0 } : {}}
+          >
+            {name}
+          </motion.h2>
         </motion.div>
-    )
-
-   
+        <div className="card-hover-text"> 카드 더보기 &gt; </div>
+      </div>
+    </motion.div>
+  );
 }
 
 // 타입 검증 경고메세지가 떠서 작성
 CardMainEmotionCard.propTypes = {
-    id:PropTypes.number.isRequired,
-    image: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    bgColor: PropTypes.string.isRequired, 
-    textColor: PropTypes.string.isRequired,
-    onClick : PropTypes.func.isRequired,
-    
-}
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
-export default CardMainEmotionCard
+export default CardMainEmotionCard;
--- frontend/h5/src/pages/Child/ChildCardDetailsPage.jsx
+++ frontend/h5/src/pages/Child/ChildCardDetailsPage.jsx
@@ -1,31 +1,34 @@
-import CardDetailsLayout from '../../components/Child/Card/CardDetailsLayout';
-import { useLocation } from 'react-router-dom';
+import CardDetailsLayout from "../../components/Child/Card/CardDetailsLayout";
+import { useLocation } from "react-router-dom";
 
 function ChildCardDetailsPage() {
+  // 현재 페이지의 위치 가져오기
+  const location = useLocation();
+  const emotion = location.state?.emotion;
 
-    // 현재 페이지의 위치 가져오기 
-    const location = useLocation();
-    const emotion = location.state?.emotion;
+  const emotionContent = () => {
+    console.log(emotion);
 
-    const emotionContent = () => {
-        console.log(emotion)
-
-        switch(emotion.type) {
-            case 'joy':
-                return(
-                    <div className='ch-joy-detail-content'>
-                        1
-                    </div>
-                );
-            }
+    switch (emotion.type) {
+      case "joy":
+        return <div className="ch-joy-detail-content"></div>;
+      case "sadness":
+        return <div className="ch-sadness-detail-content"></div>;
+      case "anger":
+        return <div className="ch-anger-detail-content"></div>;
+      case "fear":
+        return <div className="ch-fear-detail-content"></div>;
+      case "surprise":
+        return <div className="ch-surprise-detail-content"></div>;
     }
+  };
 
-    return (
-        <div>
-            <CardDetailsLayout emotion={emotion}/>
-            {emotionContent()}
-        </div>
-    );
+  return (
+    <div>
+      <CardDetailsLayout emotion={emotion} />
+      {emotionContent()}
+    </div>
+  );
 }
 
-export default ChildCardDetailsPage
+export default ChildCardDetailsPage;
