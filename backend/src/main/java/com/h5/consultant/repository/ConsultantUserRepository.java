de9cefbcb77501d353da616a17f0edc851e68446
정찬환 <chanhoan01@gmail.com>
2025-02-04 13:39:22 +0900

    feat: refreshToken 관련 기능 수정 (BE-100)

--- backend/src/main/java/com/h5/consultant/repository/ConsultantUserRepository.java
+++ backend/src/main/java/com/h5/consultant/repository/ConsultantUserRepository.java
@@ -2,11 +2,7 @@ package com.h5.consultant.repository;
 
 import com.h5.consultant.entity.ConsultantUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
--- backend/src/main/java/com/h5/parent/repository/ParentUserRepository.java
+++ backend/src/main/java/com/h5/parent/repository/ParentUserRepository.java
@@ -2,10 +2,6 @@ package com.h5.parent.repository;
 
 import com.h5.parent.entity.ParentUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
