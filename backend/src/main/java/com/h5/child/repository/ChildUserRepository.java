5c9cc84ed2a32a868a7c9296cf369b9405e97827
정찬환 <chanhoan01@gmail.com>
2025-02-06 13:56:23 +0900

    feat: 아이디 중복확인, 상담일정 아동 검색, 아동 정보 수정 기능 구현 (BE-100)

--- backend/src/main/java/com/h5/child/repository/ChildUserRepository.java
+++ backend/src/main/java/com/h5/child/repository/ChildUserRepository.java
@@ -2,14 +2,12 @@ package com.h5.child.repository;
 
 import com.h5.child.entity.ChildUserEntity;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
