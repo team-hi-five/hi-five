fcde87649122249b7001ff85cc0529141dcda70a
정찬환 <chanhoan01@gmail.com>
2025-02-09 03:18:44 +0900

    fix: 파일 업로드 관련 수정

--- backend/src/main/java/com/h5/file/service/FileService.java
+++ backend/src/main/java/com/h5/file/service/FileService.java
@@ -1,6 +1,5 @@
 package com.h5.file.service;
 
-import com.h5.file.dto.request.FileUploadRequestDto;
 import com.h5.file.dto.response.GetFileUrlResponseDto;
 import com.h5.file.entity.FileEntity;
 import org.springframework.core.io.Resource;
