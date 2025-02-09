48b299cfd22f4b28451a3953dbe8e56fdda5db6c
UiHyun Kim <keh0885@gmail.com>
2025-02-09 19:08:52 +0900

    fix: 요청 dto 수정 (BE-800)

--- backend/src/main/java/com/h5/asset/dto/request/LoadAssetByStageDto.java
+++ backend/src/main/java/com/h5/asset/dto/request/LoadAssetByStageDto.java
@@ -1,11 +1,11 @@
 package com.h5.asset.dto.request;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class LoadAssetByStageDto {
     private int chapter;
