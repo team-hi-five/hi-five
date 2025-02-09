84ec2967b0ac8b40afeaa690f86585fa5fdd4c3f
정찬환 <chanhoan01@gmail.com>
2025-02-09 22:32:21 +0900

    Update backend/src/main/java/com/h5/asset/controller/AssetController.java

--- backend/src/main/java/com/h5/asset/controller/AssetController.java
+++ backend/src/main/java/com/h5/asset/controller/AssetController.java
@@ -15,28 +15,28 @@ import org.springframework.web.bind.annotation.*;
 public class AssetController {
     private final AssetService assetService;
 
-    @PostMapping("/load-asset")
-    public ResponseEntity<?> getAsset(@RequestBody LoadAssetRequestDto loadAssetRequestDto) {
+    @GetMapping("/load-asset")
+    public ResponseEntity<?> getAsset(@ModelAttribute LoadAssetRequestDto loadAssetRequestDto) {
         return ResponseEntity.ok(assetService.loadAsset(loadAssetRequestDto));
     }
 
-    @PostMapping("/load-cards")
-    public ResponseEntity<?> getCards(@RequestBody LoadCardRequestDto loadCardRequestDto) {
+    @GetMapping("/load-cards")
+    public ResponseEntity<?> getCards(@ModelAttribute LoadCardRequestDto loadCardRequestDto) {
         return ResponseEntity.ok(assetService.loadCards(loadCardRequestDto));
     }
 
-    @PostMapping("/load-stage-asset")
-    public ResponseEntity<?> getAssetByStage(@RequestBody LoadAssetByStageDto loadAssetByStageDto) {
+    @GetMapping("/load-stage-asset")
+    public ResponseEntity<?> getAssetByStage(@ModelAttribute LoadAssetByStageDto loadAssetByStageDto) {
         return ResponseEntity.ok(assetService.loadAssetByStage(loadAssetByStageDto));
     }
 
-    @PostMapping("/load-chapter-asset")
-    public ResponseEntity<?> getChapterAsset(@RequestBody LoadAssetRequestDto loadAssetRequestDto) {
+    @GetMapping("/load-chapter-asset")
+    public ResponseEntity<?> getChapterAsset(@ModelAttribute LoadAssetRequestDto loadAssetRequestDto) {
         return ResponseEntity.ok(assetService.loadChapterAsset(loadAssetRequestDto));
     }
 
-    @PostMapping("/load-study-asset")
-    public ResponseEntity<?> getStudyAsset(@RequestBody LoadStudyAssetRequestDto loadStudyAssetRequestDto){
+    @GetMapping("/load-study-asset")
+    public ResponseEntity<?> getStudyAsset(@ModelAttribute LoadStudyAssetRequestDto loadStudyAssetRequestDto){
         return ResponseEntity.ok(assetService.loadStudyAsset(loadStudyAssetRequestDto));
     }
 
