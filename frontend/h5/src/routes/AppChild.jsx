bc874c9893bc59878543f770218e8c9ff8720d4c
seorin66 <seorin66@naver.com>
2025-02-09 23:16:36 +0900

    chore: AI 관련 라이브러리 설치 및 게임 기능개발중 (FE-620)

--- frontend/h5/src/routes/AppChild.jsx
+++ frontend/h5/src/routes/AppChild.jsx
@@ -7,17 +7,22 @@ import ChildCardMainPage from "../pages/Child/ChildCard/ChildCardMainPage";
 import ChildChatbotPage from "../pages/Child/ChildChatbotPage";
 import ChildCardDetailsPage from "../pages/Child/ChildCard/ChildCardDetailsPage";
 
-
 function AppChild() {
   return (
     <Routes>
       <Route path=":childId" element={<ChildMainPage />} />
-      <Route path="todayclass" element={<ChildClassPage />} />
-      <Route path="review" element={<ChildReviewPage />} />
-      <Route path="review/game" element={<ChildReviewGamePage />} />
-      <Route path="cardmain" element={<ChildCardMainPage />} />
-      <Route path="cardmain/details" element={<ChildCardDetailsPage />} />
-      <Route path="chatbot" element={<ChildChatbotPage />} />
+      <Route path=":childId/todayclass" element={<ChildClassPage />} />
+      <Route path=":childId/review" element={<ChildReviewPage />} />
+      <Route
+        path=":childId/review/:chapterId"
+        element={<ChildReviewGamePage />}
+      />
+      <Route path=":childId/cardmain" element={<ChildCardMainPage />} />
+      <Route
+        path=":childId/cardmain/details/:emotionType"
+        element={<ChildCardDetailsPage />}
+      />
+      <Route path=":childId/chatbot" element={<ChildChatbotPage />} />
     </Routes>
   );
 }
