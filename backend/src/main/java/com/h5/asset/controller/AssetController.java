4347821daea3cb97790a4c8b2b67f983343c65e2
UiHyun Kim <keh0885@gmail.com>
2025-02-09 19:38:18 +0900

    fix: 요청 컨트롤러 (BE-800)

--- backend/src/main/java/com/h5/asset/controller/AssetController.java
+++ backend/src/main/java/com/h5/asset/controller/AssetController.java
@@ -15,28 +15,28 @@ import org.springframework.web.bind.annotation.*;
 public class AssetController {
     private final AssetService assetService;
 
-    @GetMapping("/load-asset")
-    public ResponseEntity<?> getAsset(@ModelAttribute LoadAssetRequestDto loadAssetRequestDto) {
+    @PostMapping("/load-asset")
+    public ResponseEntity<?> getAsset(@RequestBody LoadAssetRequestDto loadAssetRequestDto) {
         return ResponseEntity.ok(assetService.loadAsset(loadAssetRequestDto));
     }
 
-    @GetMapping("/load-cards")
-    public ResponseEntity<?> getCards(@ModelAttribute LoadCardRequestDto loadCardRequestDto) {
+    @PostMapping("/load-cards")
+    public ResponseEntity<?> getCards(@RequestBody LoadCardRequestDto loadCardRequestDto) {
         return ResponseEntity.ok(assetService.loadCards(loadCardRequestDto));
     }
 
-    @GetMapping("/load-stage-asset")
-    public ResponseEntity<?> getAssetByStage(@ModelAttribute LoadAssetByStageDto loadAssetByStageDto) {
+    @PostMapping("/load-stage-asset")
+    public ResponseEntity<?> getAssetByStage(@RequestBody LoadAssetByStageDto loadAssetByStageDto) {
         return ResponseEntity.ok(assetService.loadAssetByStage(loadAssetByStageDto));
     }
 
-    @GetMapping("/load-chapter-asset")
-    public ResponseEntity<?> getChapterAsset(@ModelAttribute LoadAssetRequestDto loadAssetRequestDto) {
+    @PostMapping("/load-chapter-asset")
+    public ResponseEntity<?> getChapterAsset(@RequestBody LoadAssetRequestDto loadAssetRequestDto) {
         return ResponseEntity.ok(assetService.loadChapterAsset(loadAssetRequestDto));
     }
 
-    @GetMapping("/load-study-asset")
-    public ResponseEntity<?> getStudyAsset(@ModelAttribute LoadStudyAssetRequestDto loadStudyAssetRequestDto){
+    @PostMapping("/load-study-asset")
+    public ResponseEntity<?> getStudyAsset(@RequestBody LoadStudyAssetRequestDto loadStudyAssetRequestDto){
         return ResponseEntity.ok(assetService.loadStudyAsset(loadStudyAssetRequestDto));
     }
 
