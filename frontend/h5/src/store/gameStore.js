bede0fa92bb3faf61d5c19751f9dde5c8cb1d044
seorin66 <seorin66@naver.com>
2025-02-12 08:37:03 +0900

    feat: 아동 학습,복습 게임기능, 웹소켓 기능 추가 (FE-620)

--- frontend/h5/src/store/gameStore.js
+++ frontend/h5/src/store/gameStore.js
@@ -10,17 +10,21 @@ const useGameStore = create((set, get) => ({
     try {
       // 초기 상태
       // 한번에 모든 스테이지 데이터 가져오기
-      const stagePromises = Array.from({length:5},(_, i)=>reviewGame(chapterId, i+1));
-      
+      const stagePromises = Array.from({ length: 5 }, (_, i) =>
+        reviewGame(chapterId, i + 1)
+      );
+
       const stageResults = await Promise.all(stagePromises);
-      const chapterData = stageResults.map((res, index) => res ? { ...res, gameStageId: index + 1 } : null).filter(Boolean);
+      const chapterData = stageResults
+        .map((res, index) => (res ? { ...res, gameStageId: index + 1 } : null))
+        .filter(Boolean);
 
       // zustand에서 상태를 업데이트할때 사용(상태 저장)
-      set(({
+      set({
         chapterData: { ...get().chapterData, [chapterId]: chapterData },
         currentChapter: chapterId,
         currentStageIndex: 0,
-      }));
+      });
 
       return chapterData;
     } catch (error) {
@@ -38,11 +42,13 @@ const useGameStore = create((set, get) => ({
   // 다음 스테이지로 이동
   // 한번에 모든 데이터를 가져오지만 다음 스테이지로 이동할때 사용
   incrementStage: () =>
-    set((state) => {
-      const maxIndex = (state.chapterData[state.currentChapter]?.length || 1) - 1;
-      const newIndex = Math.min(state.currentStageIndex + 1, maxIndex);
-      return { currentStageIndex: newIndex };
-    }),
+    set((state) => ({
+      currentStageIndex:
+        state.currentStageIndex ===
+        state.chapterData[state.currentChapter].length - 1
+          ? state.currentStageIndex
+          : state.currentStageIndex + 1,
+    })),
 
   // 챕터 설정(초기화)
   setCurrentChapter: (chapterId) =>
