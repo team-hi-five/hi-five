77ecfb856f677aa94ceaf172363d11b06af7d8ed
정찬환 <chanhoan01@gmail.com>
2025-02-13 02:05:37 +0900

    feat: 권한 체인 구성

--- backend/src/main/java/com/h5/global/config/SecurityConfig.java
+++ backend/src/main/java/com/h5/global/config/SecurityConfig.java
@@ -53,10 +53,36 @@ public class SecurityConfig {
         // 필터 설정 및 경로별 권한 설정
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/webjars/**").permitAll()
-                        .requestMatchers("/auth/login").permitAll() // 로그인 API
+                        // 인증 정보가 없어도 가능
+                        .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/user/consultant/find-id", "/user/consultant/temp-pwd").permitAll()
                         .requestMatchers("/user/parent/find-id", "/user/parent/temp-pwd").permitAll()
+
+                        // 상담사만 가능
+                        .requestMatchers("/user/consultant/change-pwd", "/user/consultant/get-child",
+                                "/user/consultant/email-check","/user/consultant/search-child/",
+                                "/user/consultant/get-my-children", "/user/consultant/modify-child",
+                                "/user/consultant/register-parent-account", "/user/consultant/my-profile").hasRole("ROLE_CONSULTANT")
+                        .requestMatchers("/user/delete/approve", "/user/delete/reject", "/user/delete/get-my-delete").hasRole("ROLE_CONSULTANT")
+                        .requestMatchers("/faq/write", "/faq/delete/", "/faq/update").hasRole("ROLE_CONSULTANT")
+                        .requestMatchers("/notice/write","/notice/delete/","/notice/update").hasRole("ROLE_CONSULTANT")
+                        .requestMatchers("/qna/write-qna-comment","/qna/update-comment","/qna/delete-comment/").hasRole("ROLE_CONSULTANT")
+                        .requestMatchers("/schedule/create","/schedule/update","/schedule/delete/",
+                                "/schedule/list-by-date","/schedule/available-times").hasRole("ROLE_CONSULTANT")
+
+                        // 학부모만 가능
+                        .requestMatchers("/user/parent/change-pwd","/user/parent/my-page",
+                                "/user/parent/my-children").hasRole("ROLE_PARENT")
+                        .requestMatchers("/qna/write","/qna/update").hasRole("ROLE_PARENT")
+                        .requestMatchers("/chatbot/insert-chatbot").hasRole("ROLE_PARENT")
+                        .requestMatchers("/user/delete/request").hasRole("ROLE_PARENT")
+                        .requestMatchers("/game/start-game-chapter","/game/start-game-stage",
+                                "/game/end-game-chapter","/game/save-log").hasRole("ROLE_PARENT")
+                        .requestMatchers("study/start-study-chapter","/study/start-study-stage",
+                                "/study/end-study-chapter","/study/save-log").hasRole("ROLE_PARENT")
+                        .requestMatchers("/schedule/list-by-parent","/schedule/dates-by-parent").hasRole("ROLE_PARENT")
+
+                        // 인증된 사용자 전부 가능
                         .anyRequest().authenticated()
                 );
 
