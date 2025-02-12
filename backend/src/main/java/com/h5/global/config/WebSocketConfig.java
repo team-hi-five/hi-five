ce8f2bf49fd8d797bc03eededcf273f0a9f1d9bd
정찬환 <chanhoan01@gmail.com>
2025-02-12 16:37:50 +0900

    feat: websocket 기능 구현

--- backend/src/main/java/com/h5/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/h5/global/config/WebSocketConfig.java
@@ -11,17 +11,15 @@ import org.springframework.web.socket.config.annotation.*;
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final SessionWebSocketHandler sessionWebSocketHandler;
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/queue","/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/game")
+        registry.addEndpoint("/ws")
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
---
+++ backend/src/main/java/com/h5/global/interceptor/CustomHandshakeInterceptor.java
@@ -0,0 +1,26 @@
+package com.h5.global.interceptor;
+
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+public class CustomHandshakeInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        String authorization = request.getHeaders().getFirst("Authorization");
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            attributes.put("Authorization", authorization.substring(7));
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
