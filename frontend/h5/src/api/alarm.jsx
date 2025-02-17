5a4f939401bc08157fd04f458e0436fb317eedbb
정찬환 <chanhoan01@gmail.com>
2025-02-17 23:50:57 +0900

    fix: 알람 기능 구현 중

---
+++ frontend/h5/src/api/alarm.jsx
@@ -0,0 +1,20 @@
+import api from "./api";
+
+export const sendAlarm = async (alarmDto) => {
+    try {
+        console.log("📢 알람 발송 : ", alarmDto);
+
+        const response = await api.post("/alarm/", {
+            alarmDto
+        });
+        console.log("✅ 알람 발송 성공:", response.data);
+        return response.data;
+
+    } catch (error) {
+        console.error(
+            "❌ 알람 발송 실패:",
+            error.response ? error.response.data : error.message
+        );
+        throw error;
+    }
+};
